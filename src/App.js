923fc6fe4dc9201ca41c8477c7a4e5506d34d78e
Travis <travisbly@gmail.com>
2022-06-15 21:18:30 -0700

    added changes to router

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
+import { useStateContext } from './contexts/ContextProvider';
+
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import {
   Ecommerce,
@@ -27,7 +29,7 @@ import {
 import './App.css';
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
 
   return (
     <div>
